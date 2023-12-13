7c6c1a07f3c2a79552e3e7e543251241b0bad400
Carlos <calos.dudu1502@gmail.com>
2023-12-13 19:49:40 -0300

    first commit

---
+++ src/main/java/com/carlos/customer/registration/api/CustomerRegistrationApiApplication.java
@@ -0,0 +1,13 @@
+package com.carlos.customer.registration.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerRegistrationApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerRegistrationApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/carlos/customer/registration/api/CustomerRegistrationApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carlos.customer.registration.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerRegistrationApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
